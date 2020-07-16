a7c66e184d713fd79212f258bee30bb7f6f20190
Carlos Alcala <alcala21@gmail.com>
2020-07-15 22:38:30 -0500

    Deleted the Position class and added better handling of position coordinates with the list of empty positions.

--- tictactoe.py
+++ tictactoe.py
@@ -1,29 +1,29 @@
 import random
 
-
-class Position:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-
 class Board:
     def __init__(self):
-        self.cells = dict()
+        self.used_cells = dict()
+        self.empty_cells = [(i, j) for i in range(1, 4) for j in range(1, 4)]
 
     def print(self):
         border = ['-' * 9]
-        frame = border + [" ".join(['|'] + [self.get_value(Position(i, j)) for i in range(1, 4)] + ['|']) for j in reversed(range(1, 4))] + border
+        frame = border + [" ".join(['|'] + [self.get_value((i, j)) for i in range(1, 4)] + ['|']) for j in reversed(range(1, 4))] + border
         print("\n".join(frame))
 
     def add_value(self, position, value):
-        self.cells[(position.x, position.y)] = value
+        self.used_cells[position] = value
+        self.empty_cells.remove(position)
 
     def get_value(self, position):
-        return self.cells.get((position.x, position.y), " ")
+        return self.used_cells.get(position, " ")
 
     def delete_value(self, position):
-        del self.cells[(position.x, position.y)]
+        del self.used_cells[position]
+        self.empty_cells.append(position)
+
+    def is_occupied(self, position):
+        if position in self.used_cells:
+            return True
 
 
 class Player:
@@ -31,44 +31,48 @@ class Player:
         self.value = value
         self.board = board
         self.position = None
-        self.played = False
+        self.o_value = 'O' if self.value == 'X' else 'X'
 
     def take_input(self):
         pass
 
     def make_move(self):
         self.board.add_value(self.position, self.value)
-        self.played = not self.played
 
     def check_win(self, value):
-        if any([all([self.board.get_value(Position(i, j)) == value for i in range(1, 4)]) for j in range(1, 4)]):
+        if any([all([self.board.get_value((i, j)) == value for i in range(1, 4)]) for j in range(1, 4)]):
             return True
-        if any([all([self.board.get_value(Position(i, j)) == value for j in range(1, 4)]) for i in range(1, 4)]):
+        if any([all([self.board.get_value((i, j)) == value for j in range(1, 4)]) for i in range(1, 4)]):
             return True
-        if all([self.board.get_value(Position(i, i)) == value for i in range(1, 4)]):
+        if all([self.board.get_value((i, i)) == value for i in range(1, 4)]):
             return True
-        if all([self.board.get_value(Position(i, 4 - i)) == value for i in range(1, 4)]):
+        if all([self.board.get_value((i, 4 - i)) == value for i in range(1, 4)]):
             return True
         return False
 
     def play(self):
+        if self.__str__():
+            print(self.__str__())
         self.take_input()
         self.make_move()
         self.board.print()
         if self.check_win(self.value):
             print(self.value, "wins\n")
             return True
-        if len(self.board.cells) == 9:
+        if len(self.board.used_cells) == 9:
             print("Draw\n")
             return True
         return False
 
     def set_random_input(self):
         while True:
-            loc_position = Position(random.randint(1, 3), random.randint(1, 3))
+            loc_position = (random.randint(1, 3), random.randint(1, 3))
             if self.board.get_value(loc_position) == ' ':
                 self.position = loc_position
                 break
+
+    def __str__(self):
+        pass
 
 
 class User(Player):
@@ -79,50 +83,40 @@ class User(Player):
                 x, y = int(input_val[0]), int(input_val[1])
                 if any(x < 1 or x > 3 for x in [x, y]):
                     print("Coordinates should be from 1 to 3!")
-                elif self.board.get_value(Position(x, y)) != ' ':
+                elif self.board.is_occupied((x, y)):
                     print("This cell is occupied! Choose another one!")
                 else:
-                    self.position = Position(x, y)
+                    self.position = (x, y)
                     break
             else:
                 print("You should enter numbers!")
 
 
 class Easy(Player):
+    def __str__(self):
+        return "Making move level \"easy\"\n"
+
     def take_input(self):
-        print("Making move level \"easy\"\n")
         self.set_random_input()
 
 
 class Medium(Player):
-    def take_input(self):
-        print("Making move level \"medium\"\n")
+    def __str__(self):
+        return "Making move level \"medium\"\n"
 
+    def take_input(self):
         if not self.check_move_ahead():
             self.set_random_input()
 
     def check_move_ahead(self):
-        return self.one_move_win() or self.opponent_win()
+        return self.one_move_win(self.value) or self.one_move_win(self.o_value)
 
-    def one_move_win(self):
-        for i in range(1, 4):
-            for j in range(1, 4):
-                t_pos = Position(i, j)
-                if self.board.get_value(t_pos) == ' ' and \
-                        self.test_win(t_pos, self.value):
-                    self.board.add_value(t_pos, self.value)
-                    return True
-        return False
-
-    def opponent_win(self):
-        for i in range(1, 4):
-            for j in range(1, 4):
-                t_pos = Position(i, j)
-                if self.board.get_value(t_pos) == ' ':
-                    o_value = 'O' if self.value == 'X' else 'X'
-                    if self.test_win(t_pos, o_value):
-                        self.board.add_value(t_pos, self.value)
-                        return True
+    def one_move_win(self, value):
+        for (i, j) in self.board.empty_cells[:]:
+            t_pos = (i, j)
+            if self.test_win(t_pos, value):
+                self.position = t_pos
+                return True
         return False
 
     def test_win(self, position, value):
@@ -131,13 +125,21 @@ class Medium(Player):
         self.board.delete_value(position)
         return is_win
 
+class Hard(Player):
+    def __str__(self):
+        return "Making move level \"hard\"\n"
+
+    def take_input(self):
+        self.set_random_input()
+
 
 class Game:
     def __init__(self):
         self.player1 = None
         self.player2 = None
         self.board = Board()
-        self.player_modes = {'easy': Easy, 'user': User, 'medium': Medium}
+        self.player_modes = {'user': User, 'easy': Easy,
+                             'medium': Medium, 'hard': Hard}
 
     def play(self):
         while self.select_players():
